1b757e58b9a1e32f2e20038a7d28eb2f8b057942
mar69287 <marcoa.ruiz97@gmail.com>
2023-04-18 15:56:13 -0700

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,10 @@ useData<Game>('/games',
 { 
     params: { 
         genres: gameQuery.genre?.id, 
-        platforms: gameQuery.platform?.id}}, 
-    [gameQuery])
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder
+    }
+}, 
+[gameQuery])
 
 export default useGames;
